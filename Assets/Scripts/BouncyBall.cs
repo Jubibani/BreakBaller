aea22bed2964c8b6d8b6ea515c19824029ce55a6
Jubibani <strawberryloli3@gmail.com>
2025-04-23 16:30:49 +0800

    feat(bouncyball): reset back to the viewport when the ball falls

--- Assets/Scripts/BouncyBall.cs
+++ Assets/Scripts/BouncyBall.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class BouncyBall : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //bouncy ball's rigid body
+    private Rigidbody2D rigidBody2D;
+    
+    //line Y axis  indicator where if the ball passes this value, the game resets.
+    public float minimumY = -5.5f;
     void Start()
     {
-        
+        //reference the rigid body of the bouncy ball from the added components
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        //if the ball has its transform position value is lesser than the minimumY
+        if (transform.position.y < minimumY)
+        {
+            //then we spawn the ball's position back to the viewport where it is visible
+            transform.position = Vector3.zero;
+            rigidBody2D.linearVelocity = Vector3.zero;
+        }
+      
     }
 }
